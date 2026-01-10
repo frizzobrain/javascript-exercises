e93b40618a9d5e11d9a4493cc54e7c669ed630e6
FrizzoBrain <frizzobrain@gmail.com>
2026-01-10 14:41:31 +0100

    Complete the assignment

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+function repeatString(word, times) {
+    let string= '';
+    if (times<0)
+        return "ERROR";
+    for (let counter=0; counter<times; counter++){
+        string += word;
+    }
+    return string
+    }
 
-};
+
 
 // Do not edit below this line
 module.exports = repeatString;
