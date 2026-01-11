ad76b887d6e255de9ce36168b49ac6a87c6a79dc
FrizzoBrain <frizzobrain@gmail.com>
2026-01-11 13:43:59 +0100

    Complete exercise passing all tests

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-};
+function removeFromArray(array, ...parameters) {
+    let elementToControl;
+    let arraySliced = array;
+        for (let i=0; i<array.length; i++){
+            elementToControl = array[i];
+            console.log("l'elemento è "+elementToControl);
+            for (element of parameters){
+                if (elementToControl === element){
+                arraySliced= arraySliced.filter((element) => element!== elementToControl); 
+                console.log("guarda che bello questo array "+arraySliced);
+                }
+            }
+        }
+console.log(arraySliced);
+return arraySliced;
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
