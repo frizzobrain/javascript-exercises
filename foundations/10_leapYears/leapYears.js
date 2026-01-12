1bd8be470dc8da54e681225101be08139179121c
FrizzoBrain <frizzobrain@gmail.com>
2026-01-12 11:34:56 +0100

    Pass all the tests

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+//leap year return true when the year passed for parameter to the funcion
+//can be divided by 4. if last 2 numbers of the year are 00, the year must be divided by 400
+//to be considered leap year
+
+//function leapYear (year)
+//if number cant be divided by four
+//return false
+//else if last two numbers of the year are 00 && cant be divided by 400
+// return false
+//else return true
+
+const leapYears = function(year) {
+    if (year % 4 !=0)
+        return false;
+    else if (year % 100 == 0o0 && year % 400 !=0)
+        return false;
+    else return true;
 
 };
 
