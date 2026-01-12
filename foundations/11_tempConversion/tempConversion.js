84c03e08ebfbbdd0ff0284c639d23cc575e520de
FrizzoBrain <frizzobrain@gmail.com>
2026-01-12 17:42:16 +0100

    Pass all the tests

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+console.log(Math.round((fahrenheit-32)*5/9*10)/10);
+  return Math.round((fahrenheit-32)*5/9*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  console.log(Math.round((celsius*9/5+32)*10)/10);
+  return Math.round((celsius*9/5+32)*10)/10;
+  
 };
 
 // Do not edit below this line
