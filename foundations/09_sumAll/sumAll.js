c3188ec717103ea78de31c0d3b87a38d967988de
FrizzoBrain <frizzobrain@gmail.com>
2026-01-12 10:58:20 +0100

    Pass all the tests

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,33 @@
-const sumAll = function() {
+//create a function that takes 2 numbers as it's parameters
+//it must sum all the number between this two, including them and then return the sum
+
+//funcion sumAll (a,b)
+//if a > b the iteration goes from a to b incrementing by 1)
+//else the iteration goes from b to a incrementing by 1
+//each iteration add to sum the current value of the iterator
+//when it exits from the loop, it returns the sum value
+
+const sumAll = function(valStart, valFinish) {
+    let iterator;
+    let sum=0;
+    if (valStart<0 || 
+        valFinish<0 || 
+        !Number.isInteger(valStart) ||
+        !Number.isInteger(valFinish))
+        return "ERROR";
+    //add a check to see if parameters are integer also
+    if (valStart<= valFinish){
+        for (iterator=valStart; iterator<=valFinish; iterator++){
+            //console.log("iterator value" + iterator);
+            sum+=iterator;
+        }
+
+    } else {
+        for (iterator=valFinish; iterator<=valStart; iterator++){
+            sum+=iterator;
+        }
+    }
+return sum;
 
 };
 
