21e4c2bf03e5b942c99cde40218867689508f130
FrizzoBrain <frizzobrain@gmail.com>
2026-01-21 15:04:49 +0100

    Pass all the test

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+const punctuationless = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?'\s]/g, "");
+const array = punctuationless.toLowerCase().split("");
+let boo = false;
+    for (let i=0; i<(array.length)/2; i++){
+        if (array[i]==array[(array.length)-1-i]){
+            boo=true;
+        }else {
+            return false;}
+    }
+return boo;
 };
 
 // Do not edit below this line
