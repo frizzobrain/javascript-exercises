b693637fe9b24aad23c90dae8eb25c48b30a2c7b
FrizzoBrain <frizzobrain@gmail.com>
2026-01-22 11:35:47 +0100

    Pass all the tests

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+let sum=0;
+let first = 0;
+let second = 1;
+if (num==0) return 0;
+if (num<0) return "OOPS";
+for (let i=0; i+1<parseInt(num); i++){
+    sum = first + second;
+    console.log(sum);
+    first= second;
+    console.log(first);
+    second = sum;
+    console.log(second);
+}
+return second;
 };
 
 // Do not edit below this line
