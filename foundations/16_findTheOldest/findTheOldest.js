716c5e05438ff90a8fc24fb629a58e31511b84dd
FrizzoBrain <frizzobrain@gmail.com>
2026-01-22 14:24:25 +0100

    Pass all the tests

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+function getAge (person){
+    if (person.yearOfDeath){
+        return (person.yearOfDeath-person.yearOfBirth);
+    }else return ((new Date().getFullYear())-person.yearOfBirth);
+}
 
+const findTheOldest = function(arr) {
+let people = arr.sort((a,b) => (getAge(b)-(getAge(a))));
+console.log(people[0]);
+if (people[0]){
+    return people[0];
+}else return people[1];
 };
 
 // Do not edit below this line
